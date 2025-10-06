60ae68ac853a8e20949582fac21f84a9e39f6e21
PavuksWeb <112330889+PavuksWeb@users.noreply.github.com>
2025-10-06 17:30:32 +0300

    Update limiter.module.ts

--- backend/src/limiter/limiter.module.ts
+++ backend/src/limiter/limiter.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { LimiterService } from './limiter.service';
 import { LimiterController } from './limiter.controller';
 
 @Module({
-  providers: [LimiterService],
   controllers: [LimiterController],
 })
 export class LimiterModule {}
